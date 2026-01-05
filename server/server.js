a7d099b6c2d99a69aa8e09a214a65d1afb059d96
tiapetts <tpetts@vintagepearl.world>
2026-01-04 20:57:05 -0600

    setting up db connection

--- server/server.js
+++ server/server.js
@@ -6,6 +6,19 @@ const express = require('express');
 const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const e = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// database connection
+const db = new sqlite3.Database(
+    path.join(__dirname, 'second_chance_paws.db'),
+    (err) => {
+        if (err) {
+            console.error('Error connecting to database:', err.message);
+        } else {
+            console.log('Connected to the Second Chance Paws database.');
+        }  
+    } 
+);
