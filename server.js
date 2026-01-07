538bf272346825a6d1677f0e508120a12c130fd6
tiapetts <tpetts@vintagepearl.world>
2026-01-06 20:59:36 -0600

    lets try this again

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'second_chance_paws.db')));
 
 // database connection
 const db = new sqlite3.Database(dbPath, (err) => {
