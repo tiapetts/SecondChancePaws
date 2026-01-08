a4b85bd2a119f06e6b37b336b0a4bf812c09b02b
tiapetts <tpetts@vintagepearl.world>
2026-01-07 23:54:29 -0600

    including seed data for animals

---
+++ db/seed_animals.sql
@@ -0,0 +1,8 @@
+INSERT INTO animals (id, name, species, age, adopted) VALUES
+(1, 'Bella', 'Dog', 3, 0),
+(2, 'Max', 'Dog', 5, 1),
+(3, 'Luna', 'Cat', 2, 0),
+(4, 'Oliver', 'Cat', 4, 1),
+(5, 'Charlie', 'Rabbit', 1, 0),
+(6, 'Daisy', 'Dog', 6, 0),
+(7, 'Milo', 'Cat', 3, 0);
